58d9d2b03458b80135ab008cecb7798c71b70465
florchao <fchao@itba.edu.ar>
2021-05-30 17:12:33 -0400

    Co-authored-by: ssandrini <ssandrini@users.noreply.github.com> Co-authored-by: skonfederak <solkonfe@users.noreply.github.com>

--- Kernel/interruptions/exceptions.c
+++ Kernel/interruptions/exceptions.c
@@ -1,17 +1,39 @@
 #include <naiveConsole.h>
 #define ZERO_EXCEPTION_ID 0
+#define INVALID_OPERATION_ID 6
 
 static void zero_division();
 
 void exceptionDispatcher(int exception, uint64_t * stackFrame) {
 	if (exception == ZERO_EXCEPTION_ID)
 		zero_division(stackFrame);
-	ncPrint("HOLA",0);
+	if(exception == INVALID_OPERATION_ID)
+		invalid_operation(stackFrame);
+	//habria que imprimit los registros
+	//hay que resetear la pantalla
 	return;
+
+	//en el main del kernel se envian los valores iniciales del RSP y de la primera y 
+	//se ejecuta la instruccion para el reinicio luego de una excepcion: setAddresses((uint64_t *)sampleCodeModuleAddress, getRSP());
+
+	//En lib.asm
+	//getRSP:
+	//mov rax, rsp
+	//ret
+
+	//void setAddresses(uint64_t * ip, uint64_t * rsp) {
+	//ipReturn = ip;
+	//rspReturn = rsp;
+	//}
+}
+
+static void invalid_operation(uint64_t * stackFrame){
+	ncPrint("Invalid code of operation", 0);
+	ncNewline();
 }
 
 static void zero_division(uint64_t * stackframe) {
-	ncPrint("zero division error ",0);
+	ncPrint("Zero division error ",0);
 	ncNewline();
 }
 
