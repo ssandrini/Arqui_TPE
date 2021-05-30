ecceba4769332541bce64a766fa0a5a5ff98208f
ssandrini2 <ssandrini@itba.edu.ar>
2021-05-29 23:01:43 -0300

    primer programa de quadratic, esta probado solo y funciona, nos falta agregar el soporte del printf para long double y hacer el scanf

--- Userland/SampleCodeModule/asm/libasm.asm
+++ Userland/SampleCodeModule/asm/libasm.asm
@@ -4,6 +4,7 @@ GLOBAL _getTime
 GLOBAL _getReg
 GLOBAL _getMem
 GLOBAL _changeScreen
+GLOBAL _quadratic
 section .text
 
 _write:
@@ -72,6 +73,53 @@ _changeScreen:
     mov rax, 5   ; llamada a la syscall changeScreen
     int 80h
     
+    mov rsp, rbp
+    pop rbp
+    ret
+
+_quadratic:
+    push rbp              ; armado del stackframe
+    mov rbp, rsp
+   
+    fninit                
+    fld   tword[rdi]      ; st0 = a
+    fadd    st0           ; st0 = 2a
+    fld   tword[rsi]      ; st0 = b   ; st1 = 2a
+    fmul    st0           ; st0 = b²  ; st1 = 2a
+    fld   tword[rdx]      ; st0 = c   ; st1 = b²  ;  st2 = 2a
+    fmul    st2           ; st0 = 2ac ; st1 = b²  ;  st2 = 2a
+    fadd    st0           ; st0 = 4ac ; st1 = b²  ;  st2 = 2a
+    fsubp                 ; st0 = b²-4ac ; st1 = 2a
+    ; tengo que chequear si st0 es menor que 0
+    fldz                  ; st0 = 0.0 ; st1 = b²-4ac ; st2 = 2a
+
+    ; al finalizar la comparacion me deja los flags en EFLAGS
+    fcomip st0, st1       ; st0 = b²-4ac ; st1 = 2a
+    ja .no_results        ; si es negativo
+    ; si sigo aca es porque el discriminante era positivo
+    fsqrt                 ; st0 = raiz de b²-4ac ; st1 = 2a
+    
+    ; calculo de la primera raiz
+    fld    tword[rsi]     ; st0 = b   ; st1 = raiz de b²-4ac ; st2 = 2a
+    fchs                  ; st0 = -b   ; st1 = raiz de b²-4ac ; st2 = 2a
+    fsub    st1           ; st0 = -b - raiz de b²-4ac  ; st1 = raiz de b²-4ac ; st2 = 2a
+    fdiv    st2           ; st0 = (-b - raiz de b²-4ac) / 2a  ; st1 = raiz de b²-4ac ; st2 = 2a
+                          
+    fstp    tword[rcx]    ; st0 = raiz de b²-4ac ; st1= 2a
+    ;calculo de la segunda raiz
+    fld    tword[rsi]     ; st0 = b   ; st1 = raiz de b²-4ac ; st2 = 2a
+    fchs                  ; st0 = -b   ; st1 = raiz de b²-4ac ; st2 = 2a
+    fadd    st1           ; st0 = -b + raiz de b²-4ac  ; st1 = raiz de b²-4ac ; st2 = 2a
+    fdiv    st2           ; st0 = (-b + raiz de b²-4ac) / 2a  ; st1 = raiz de b²-4ac ; st2 = 2a
+    fstp    tword[r8]     ; st0 = raiz de b²-4ac ; st1= 2a
+
+    mov rax, 1            ; devulevo 1 porque pude calcular
+    mov rsp, rbp
+    pop rbp
+    ret
+
+.no_results:
+    mov rax, 0            ; devulevo 0 porque no pude calcular
     mov rsp, rbp
     pop rbp
     ret
