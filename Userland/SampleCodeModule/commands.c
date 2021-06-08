090bf4d630b9820e8c144ad0d08ef5c94b454bd6
florchao <fchao@itba.edu.ar>
2021-06-08 19:23:42 -0400

    esta el help

--- Userland/SampleCodeModule/commands.c
+++ Userland/SampleCodeModule/commands.c
@@ -1,12 +1,14 @@
 #include <commands.h>
 #define EPSILON 0.00000001
 char commandsNames[9][20] = {"help", "time", "inforeg", "printmem", "cpuid", "trigger0", "trigger6", "quadratic","clear"};
-char info[3][150] = {"inforeg: imprime en pantalla el valor de todos los registros \n",
+char info[8][150] = {"inforeg: imprime en pantalla el valor de todos los registros \n",
 "printmem DIR : realiza un volcado de memoria de 32 bytes a partir de la direccion recibida como argumento \n",
-"time: desplega el dia y la hora del sistema\n"};
+"time: desplega el dia y la hora del sistema\n", "trigger0: demuestra el error de division por cero\n", 
+"trigger6: demuestra el error de operacion invalida\n", "quadratic: obtiene las raices de la funcion quadratica deseada\n",
+"cpuid: despliega los features especiales del procesador\n", "clear: borra toda la pantalla\n"};
 
 void help() {
-    for(int i = 0; i < 3; i++){
+    for(int i = 0; i < 8; i++){
         printf("%s",info[i]);
     }
 }
