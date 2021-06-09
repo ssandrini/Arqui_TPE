78b6608153a0ed22fe70b3e0fb19652575021cfa
florchao <58371281+florchao@users.noreply.github.com>
2021-06-08 21:18:33 -0400

    borro comentarios

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -8,21 +8,7 @@ void testZero(){
 }
 
 int main() {
-	//printf("a ver si esto %s %d %x puntos \n","anda",10, 0xAFD56);
 	shell();
 	return 0;
-
-	//char * m = "mundo";
-	//printf("a ver si esto anda");
-	//printf("hola %s %d", m, 10);
-	/*while(1) {
-		unsigned char a = getChar();
-		if(a !=0){
-			putChar(a); 
-		}
-	}
-	*/
-	//testZero();
-
 }
 
